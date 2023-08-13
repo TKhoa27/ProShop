1e2ad69d595c7ccf669f6bbd6e4c0473dee47e7a
TKhoa27 <tieukhoa271@gmail.com>
2023-08-13 16:58:56 +0700

    add Loader, Message, and create redux

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
 import colors from "colors";
+import { ErrorHandler, NotFound } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -10,18 +11,19 @@ connectDB();
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log(req.originalUrl);
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Api is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(NotFound);
+app.use(ErrorHandler);
 
 const PORT = process.env.PORT || 5000;
 
